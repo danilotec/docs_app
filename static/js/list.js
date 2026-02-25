ff2c755a28c94cd108872fa3b8eeb930cf4a6e4f
danilotec <danilo0123499@gmail.com>
2026-02-25 15:19:35 -0300

    add news files

--- static/js/list.js
+++ static/js/list.js
@@ -15,6 +15,26 @@ const pdfList = [
         size: "225.4 KB"
     },
     {
+        name: "Atlas Copco G15 18 22 E Vsd Lista De Peças (atualizado 31 10 2023).pdf",
+        filename: "Atlas Copco - G15-18-22 e VSD - Lista de Peças (ATUALIZADO 31-10-2023).pdf",
+        size: "1.5 MB"
+    },
+    {
+        name: "Atlas Copco G15 22&vsd Diagrama De Fluxo.pdf",
+        filename: "Atlas Copco - G15-22&VSD - Diagrama de fluxo.pdf",
+        size: "27.1 KB"
+    },
+    {
+        name: "Atlas Copco G15 22vsd Diagrama Elétrico.pdf",
+        filename: "Atlas Copco - G15-22VSD - Diagrama elétrico.pdf",
+        size: "1.2 MB"
+    },
+    {
+        name: "Atlas Copco G15vsd 18vsd 22vsd Manual De Instruções.pdf",
+        filename: "Atlas Copco - G15VSD-18VSD-22VSD - Manual de Instruções.pdf",
+        size: "2.0 MB"
+    },
+    {
         name: "Atlas Copco G7l Ff G11 Ff Manual De Instruções 1.pdf",
         filename: "Atlas Copco - G7L FF - G11 FF - Manual de Instruções-1.pdf",
         size: "2.9 MB"
@@ -35,9 +55,24 @@ const pdfList = [
         size: "150.5 KB"
     },
     {
+        name: "Atlas Copco Gx7 11 2008 Manual De Instruções (1).pdf",
+        filename: "Atlas Copco - GX7-11 2008 - Manual de Instruções (1).pdf",
+        size: "3.2 MB"
+    },
+    {
         name: "Atlas Copco Gx7 11 2008 Manual De Instruções.pdf",
         filename: "Atlas Copco - GX7-11 2008 - Manual de Instruções.pdf",
         size: "3.2 MB"
+    },
+    {
+        name: "Atlas Copco Usinas De Oxigênio Opg Manual De Instruções Ogp (05 01 2023).pdf",    
+        filename: "Atlas Copco - Usinas de Oxigênio OPG - Manual de Instruções OGP (05-01-2023).pdf",
+        size: "4.9 MB"
+    },
+    {
+        name: "Atlas Copco Usinas De Oxigênio Opg Manual De Instruções Ogp.pdf",
+        filename: "Atlas Copco - Usinas de Oxigênio OPG - Manual de Instruções OGP.pdf",
+        size: "4.3 MB"
     },
     {
         name: "Atlas Copco Purificateur Air Bap (1).pdf",
@@ -110,6 +145,11 @@ const pdfList = [
         size: "3.2 MB"
     },
     {
+        name: "Manual G4 7.pdf",
+        filename: "Manual G4-7.pdf",
+        size: "3.9 MB"
+    },
+    {
         name: "Manual De Instruções Ga55+ 90 Até Serie Bqd....pdf",
         filename: "Manual de instruções GA55+_90 ATÉ SERIE BQD....pdf",
         size: "9.0 MB"
@@ -150,14 +190,49 @@ const pdfList = [
         size: "2.0 MB"
     },
     {
+        name: "Ogp 2 225 Installation Atlas Oxymat.pdf",
+        filename: "OGP 2-225 Installation atlas-oxymat.pdf",
+        size: "2.0 MB"
+    },
+    {
+        name: "Ogp 2 225 Atlas Oxymat.pdf",
+        filename: "OGP 2-225 atlas-oxymat.pdf",
+        size: "11.6 MB"
+    },
+    {
+        name: "Oxymat Inicialização Normal.pdf",
+        filename: "OXYMAT - Inicialização Normal.pdf",
+        size: "537.0 KB"
+    },
+    {
+        name: "Oxymat Usinas O10 A O1500 Manual De Instruções.pdf",
+        filename: "OXYMAT - Usinas O10 a O1500 - Manual de Instruções.pdf",
+        size: "3.0 MB"
+    },
+    {
+        name: "User Manual For Elektronikon® Graphic Controller.pdf",
+        filename: "User manual for Elektronikon® Graphic controller .pdf",
+        size: "3.0 MB"
+    },
+    {
+        name: "Vacuum Pump Dzs 065v 300p.pdf",
+        filename: "Vacuum pump dzs 065v-300p.pdf",
+        size: "2.5 MB"
+    },
+    {
         name: "Weg Cfw500 Manual De Programacao 10001469555 1 1x Manual Portugues.pdf",
-        filename: "WEG_cfw500_manual_de_programacao_10001469555_1_1x_manual_portugues.pdf",
+        filename: "WEG_cfw500_manual_de_programacao_10001469555_1_1x_manual_portugues.pdf",      
         size: "16.7 MB"
     },
     {
         name: "Weg Cfw500 Manual De Programacao 10001469555 1 8x Manual Portugues.pdf",
-        filename: "WEG_cfw500_manual_de_programacao_10001469555_1_8x_manual_portugues.pdf",
+        filename: "WEG_cfw500_manual_de_programacao_10001469555_1_8x_manual_portugues.pdf",      
         size: "8.4 MB"
+    },
+    {
+        name: "Apresentao Elektronikon Mk5 Intermediario.pdf",
+        filename: "apresentao-elektronikon-mk5-intermediario.pdf",
+        size: "1.8 MB"
     },
     {
         name: "Atlas Copco Brasil Manual Do Ar Comprimido.pdf",
@@ -173,6 +248,11 @@ const pdfList = [
         name: "Atlas Copco Zt18 90 Zr45 90 Manual.pdf",
         filename: "atlas-copco-zt18-90-zr45-90-manual.pdf",
         size: "1.1 MB"
+    },
+    {
+        name: "Esquema Eletrico G7 Yd.pdf",
+        filename: "esquema eletrico - G7 YD.pdf",
+        size: "146.3 KB"
     },
     {
         name: "Generador De Oxigenio (1).pdf",
